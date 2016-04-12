88ed2d84dd892148b09422d62bd9fd86655d38f1
catsworld <519418441@qq.com>
2016-04-12 20:23:17 +0800

    Build a base project.

---
+++ head.h
@@ -0,0 +1,43 @@
+class Setting{
+	public:
+		char name[50]={0};
+		void changeName(char *new){
+		}
+	private:
+		void output(){
+		}
+}
+class Answerlist{
+	public:
+		void answer(char *question){
+		}
+		void init(){
+		}
+		void teach(char *question,char *answer){
+		}
+	private:
+		char ques[1000][100]={0},ans[1000][100]={0};
+		int num=0;
+		void sort(int L,int r){
+		}
+		void output(){
+		}
+}
+class Record{
+	public:
+		void add(char *new){
+		}
+	private:
+		char rec[1000][200]={0};
+		int num=0;
+		void output(){
+		}
+}
+void encrypt(char *out,char *in){
+}
+void startup(){
+}
+void shutdown(){
+}
+void init(){
+}
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include<cstdlib>
+#include"head.h"
+int main(){
+	startup();
+	shutdown();
+	return 0;
+}
