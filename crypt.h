b219356ac65cd107bfe0ba314ffb3a896fe049a3
LI JIAHAO <lijiahao99131@gmail.com>
2017-04-09 19:59:20 +0800

    formatted some code

--- crypt.h
+++ crypt.h
@@ -6,8 +6,8 @@ extern const char password[];
 void encrypt(char *out,char *in){
     int tmp[60]={0};
     int k=1;
-    for(int i=0;i<strlen(in);i++){
-        k=(k+1)%strlen(password);
+    for(int i = 0; i < strlen(in); i++) {
+        k = (k + 1) % strlen(password);
     }
 	printf(out,"%s",tmp);
 }
